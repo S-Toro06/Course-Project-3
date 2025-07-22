88ff4adbbf36b5c075da1b21d81a298fa009107d
Sean <seantorossian@gmail.com>
2025-07-21 18:46:02 -0700

    adding files

---
+++ demo/src/test/java/com/example/cab/CourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
